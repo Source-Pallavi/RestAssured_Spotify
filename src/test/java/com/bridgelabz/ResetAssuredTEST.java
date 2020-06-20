90e9600197f1c5740fb4c4de082915abdd21a88a
Source-Pallavi <59175852+Source-Pallavi@users.noreply.github.com>
2020-06-20 13:49:16 +0530

    [Pallavi] Added playlist data of user

--- src/test/java/com/bridgelabz/ResetAssuredTEST.java
+++ src/test/java/com/bridgelabz/ResetAssuredTEST.java
@@ -8,11 +8,10 @@ import static io.restassured.RestAssured.given;
 
 public class ResetAssuredTEST
 {Response response;
-    private static String token = "";
+    private static String token ;
     private String playlists[];
-    private int totalPlayList;
     private String trackId[];
-    String uid = "";
+    private String userID = "";
     String JSON = "application/json";
 
 
@@ -20,11 +19,11 @@ public class ResetAssuredTEST
     public void setUp()
     {
         token=
-                "Bearer BQC80Va9aw91gG8eSDlAa_5n9RTkxgkz_vyvwKORpqShLSJJ-XJdEillqmkC7TVkUZJNGIOLuME8Nsch-bt6yJ_mWY72ONYoUMd3mwrDLRcoIbnykv3WAcnRE7glRuin7NKPSDt01fpqMk1C8xEv20RhKPzzujPD_frqzj_dWfoyezhTlbufFZXawfN-mWpDaA5cyLjfxldgILKtTb1b1me65SWtiCVNSXFfE00Xjd7UqswmZZyzFnXuACyTOTSFhjF9yWzTy5oAH_Ie5ECb_q-xzQisaXEyHXp1";
+                "Bearer BQCUfUzDZipmQ2RC-W8LRapV929CxqFm4_0yKq2GV02YBnTa5CfKg2-O0Oxj9EiHNXsylFCYhgMrnSH3JRKVdrV6X52sfpgzw_YYNLhOLF7cOPo_Yw_mnMqai2VBQcWccNS8hri2_wk49Paweuyuf0EnhSE6wuDnafP2alxDy2paFAmNOjDtdSC4XBTJRifaVcLrQpo__-Xk518KBPrT2KMiz-EcPubNee46-ByiYGKH_Ksd-KqS6fLBFujl35jo1uRnCieipmsesYQf-8ugHAodT0HKKEcj17HV";
          response = given().contentType(JSON).accept(JSON).header("Authorization", token).when().get("https://api.spotify.com/v1/me");
-        uid = response.path("id");
+        userID = response.path("id");
         response.then().assertThat().statusCode(200);
-        System.out.println(uid);
+        System.out.println(userID);
         /* to fetch the user id*/
     }
     @Test
@@ -41,15 +40,17 @@ public class ResetAssuredTEST
     }
     @Test
     public void create_PlayListSpotify() {
-         response = given()
-                .accept(JSON)
-                .contentType(JSON)
-                .header("Authorization", token)
+         response = given().accept(JSON).contentType(JSON).header("Authorization", token)
                 .body("{\"name\": \"Prad\"," +
                         "\"description\": \"New playlist description\"," +
-                        "\"public\": false}").when().post(" https://api.spotify.com/v1/users/" + uid + "/playlists");
-        String path = response.path("owner.display_name");
+                        "\"public\": false}").when().post(" https://api.spotify.com/v1/users/" + userID + "/playlists");
         System.out.println("PlayList Name:" + response.path("name"));
         response.then().assertThat().statusCode(201);
+    }
+    @Test
+    public void getUserPlayListInfo() {
+         response = given().accept(JSON).contentType(JSON).header("Authorization", token).when().get("https://api.spotify.com/v1/users/" + userID + "/playlists");
+        response.then().assertThat().statusCode(200);
+        System.out.println("Total PlayList:" +  response.path("total"));
     }
 }
