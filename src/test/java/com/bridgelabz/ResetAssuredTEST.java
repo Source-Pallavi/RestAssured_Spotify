0013a475012db3bb1ace2c47e1d6271cf0e462fb
Source-Pallavi <59175852+Source-Pallavi@users.noreply.github.com>
2020-06-22 09:47:56 +0530

    Added the Automation script for testing the spotify

--- src/test/java/com/bridgelabz/ResetAssuredTEST.java
+++ src/test/java/com/bridgelabz/ResetAssuredTEST.java
@@ -11,6 +11,9 @@ public class ResetAssuredTEST
     private static String token ;
     private String userID ;
     private String JSON = "application/json";
+    private int totalPlayList;
+    private String[] playlists;
+    private String[] trackId;
 
 
     @BeforeMethod
@@ -44,7 +47,7 @@ public class ResetAssuredTEST
         response.prettyPrint();
     }
     /*to create play list*/
-    @Test
+    @Test(priority = 3)
     public void create_PlayListSpotify() {
          response = given().accept(JSON).contentType(JSON).header("Authorization", token)
                 .body("{\"name\": \"Prad\"," +
@@ -54,11 +57,94 @@ public class ResetAssuredTEST
         response.then().assertThat().statusCode(201);
     }
     /*to find the total no of playlist is present*/
-    @Test
+    @Test(priority = 2)
     public void getPlayList() {
          response = given().accept(JSON).contentType(JSON).header("Authorization", token).when().get("https://api.spotify.com/v1/users/" + userID + "/playlists");
         response.then().assertThat().statusCode(200);
         System.out.println("Total no. of PlayList:" +  response.path("total"));
     }
+    @Test(priority = 1)
+    public void getUser_Id() {
+         response = given()
+                .contentType(JSON)
+                .accept(JSON)
+                .header("Authorization", token)
+                .when()
+                .get("https://api.spotify.com/v1/me");
+        userID = response.path("id");
+        response.then().assertThat().statusCode(200);
+        System.out.println(userID);
+        response = given()
+                .accept(JSON)
+                .contentType(JSON)
+                .header("Authorization", token)
+                .when()
+                .get("https://api.spotify.com/v1/users/" + userID + "/");
+        response.then().assertThat().statusCode(200);
+        response.prettyPrint();
+        response = given()
+                .accept(JSON)
+                .contentType(JSON)
+                .header("Authorization", token)
+                .when()
+                .get("https://api.spotify.com/v1/users/" + userID + "/playlists");
+        response.then().assertThat().statusCode(200);
+        totalPlayList = response.path("total"); //get total playlist
+        System.out.println("Total PlayList:" + totalPlayList);
+        response = given()
+                .accept(JSON)
+                .contentType(JSON)
+                .header("Authorization", token)
+                .when()
+                .get("https://api.spotify.com/v1/users/" + userID + "/playlists");
+        //response.prettyPrint();
+        playlists = new String[totalPlayList];
+        for(int i = 0; i < playlists.length; i++) {
+            playlists[i] = response.path("items[" + i + "].id");
+        }
+        for(String id : playlists) {
+            System.out.println("PlayList Id" + id);
+        }
+        response = given()
+                .accept(JSON)
+                .contentType(JSON)
+                .header("Authorization", token)
+                .when().accept(JSON)
+                .get("https://api.spotify.com/v1/playlists/" + playlists[0] + "/tracks");
+        int totalTracks = response.path("total");
+        trackId = new String[totalTracks];
+        for(int i = 0; i < trackId.length; i++) {
+            trackId[i] = response.path("items[" + i + "].track.uri"); //get uri of track
+        }
+        for(String track : trackId)
+        {
+            System.out.println("Tracks:" + track);
+        }
+        response = given()
+                .accept(JSON)
+                .contentType(JSON)
+                .header("Authorization", token)
+                .when().accept(JSON)
+                .get("https://api.spotify.com/v1/playlists/" + playlists[0] + "/tracks");
+        totalTracks = response.path("total");
+        trackId = new String[totalTracks];
+        for(int i = 0; i < trackId.length; i++) {
+            trackId[i] = response.path("items[" + i + "].track.uri");
+        }
+        for(String track : trackId) {
+            System.out.println("Tracks:" + track);
+        }
+        response = given()
+                .accept(JSON)
+                .contentType(JSON)
+                .header("Authorization", token)
+                .body("{\"uris\": [\"" + trackId[1] + "\"]}")
+                .when()
+                .delete("https://api.spotify.com/v1/playlists/" + playlists[0] + "/tracks/");
+        System.out.println("Track Id is:" + response.path("snapshot_id"));
+    }
+
+
+
 
 }
